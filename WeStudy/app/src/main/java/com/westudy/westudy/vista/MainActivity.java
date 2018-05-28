cc08e4f6223f48dc062ea1a6dc8fd71597b239e7
garconde <david.jose97@hotmail.com>
2018-05-28 06:07:18 -0500

    Avance de las vistas del proyecto

--- WeStudy/app/src/main/java/com/westudy/westudy/vista/MainActivity.java
+++ WeStudy/app/src/main/java/com/westudy/westudy/vista/MainActivity.java
@@ -1,13 +1,15 @@
-package com.westudy.westudy;
+package com.westudy.westudy.vista;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.westudy.westudy.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_activity_main);
+        setContentView(R.layout.layout_login);
     }
 }
